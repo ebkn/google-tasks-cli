050d9408b31105d4ffba9bb0601ff78502328947
ebkn <ktennis.mqekr12@gmail.com>
2019-10-14 17:25:34 +0900

    feat: get tasks cliet func

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/skratchdot/open-golang v0.0.0-20190402232053-79abb63cd66e
 	github.com/urfave/cli v1.22.1
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	google.golang.org/api v0.11.0
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 
 	"github.com/urfave/cli"
+	"google.golang.org/api/tasks/v1"
 )
 
 const credentialFile = "credentials.json"
@@ -14,17 +15,25 @@ var (
 	ctx context.Context
 )
 
+func getTasksClient() *tasks.Service {
+	token, err := tokenFromFile(credentialFile)
+	if err != nil {
+		log.Println("You should authorize with Google. Try authorize command.")
+		log.Fatalf("error: %s", err.Error())
+	}
+
+	client := config.Client(context.Background(), token)
+
+	srv, err := tasks.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve tasks Client %v", err)
+	}
+	return srv
+}
+
 func main() {
 	ctx = context.Background()
 
-	// token, err := tokenFromFile(credentialFile)
-	// if err != nil {
-	// 	log.Println("You should authorize with Google. Try authorize command.")
-	// 	log.Fatalf("error: %s", err.Error())
-	// }
-	//
-	// log.Println(token)
-	//
 	app := cli.NewApp()
 	app.Name = "google-tasks-cli"
 
